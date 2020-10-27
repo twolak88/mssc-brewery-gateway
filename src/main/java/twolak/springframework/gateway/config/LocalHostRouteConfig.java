e79f96361d9f6d1092b527347df46695f24f26bd
twolak88 <twolak88@gmail.com>
2020-10-28 00:25:59 +0100

    Add Local Host Route for Beer Service and Order Service Closes #3

---
+++ src/main/java/twolak/springframework/gateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twolak.springframework.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author twolak
+ */
+@Configuration
+public class LocalHostRouteConfig {
+    
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(r -> r.path("/api/v1/beer","/api/v1/beer/*", "/api/v1/beerUpc/*")
+                        .uri("http://localhost:8080")
+                        .id("beer-service"))
+                .route(r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081")
+                        .id("beer-order-service"))
+                .build();
+    }
+}
